94a56f78f2faeccc1bd649530a0e4b3016959b54
Hennessy <834819108@qq.com>
2019-03-07 20:45:06 +0800

    '2019-3-7'

--- Login/Caculator_Pane.py
+++ Login/Caculator_Pane.py
@@ -7,20 +7,37 @@ import sys
 
 class Caculator(QObject):
 
+    show_content=pyqtSignal(str)
+
     def __init__(self,parent):
         super().__init__(parent)
 
         #数字键位 num 运算符 operator
         self.key_models=[]
 
+    def caculate(self):
+        if len(self.key_models)>0 and self.key_models[-1]["role"]=="operator":
+            self.key_models.pop(-1)
+        expression=""
+        for model in self.key_models:
+            expression+=model["title"]
+
+        result=eval(expression)
+        print(result)
+        return result
+
     def parse_key_model(self,key_model):
         # print(key_model)
         if key_model["role"]=="clear":
             print("清空所有内容")
+            self.key_models=[]
+            self.show_content.emit("0.0")
             return None
 
         if key_model["role"]=="caculate":
             print("计算所有内容")
+            result=self.caculate()
+            self.show_content.emit(str(result))
             return None
         #num operator
         if len(self.key_models)==0:
@@ -28,6 +45,18 @@ class Caculator(QObject):
                 if key_model["title"]==".":
                     key_model["title"]="0."
                 self.key_models.append(key_model)
+                self.show_content.emit(self.key_models[-1]["title"])
+            return None
+
+        if key_model["title"] in ("%","+/-"):
+            if self.key_models[-1]["role"]!="num":
+                return None
+            else:
+                if key_model["title"]=="%":
+                    self.key_models[-1]["title"]=str(float(self.key_models[-1]["title"])/100)
+                elif key_model["title"]=="+/-":
+                    self.key_models[-1]["title"] = str(float(self.key_models[-1]["title"]) *(-1))
+                self.show_content.emit(self.key_models[-1]["title"])
             return None
 
         if key_model["role"]==self.key_models[-1]["role"]:
@@ -39,11 +68,19 @@ class Caculator(QObject):
                     self.key_models[-1]["title"]+=key_model["title"]
                 else:
                     self.key_models[-1]["title"] = key_model["title"]
+                self.show_content.emit(self.key_models[-1]["title"])
+
 
             if key_model["role"]=="operator":
                 self.key_models[-1]["title"] = key_model["title"]
-
+                self.show_content.emit(str(self.caculate()))
         else:
+            if key_model["title"] in (".","%","+/-"):
+                return None
+            if key_model["role"]=="num":
+                self.show_content.emit(key_model["title"])
+            elif key_model["role"]=="operator":
+                self.show_content.emit(str(self.caculate()))
             self.key_models.append(key_model)
 
         print(self.key_models)
@@ -60,6 +97,10 @@ class CaculatorPane(QWidget,Ui_Form):
 
         self.setupUi(self)
         self.caculator=Caculator(self)
+        self.caculator.show_content.connect(self.show_content)
+
+    def show_content(self,content):
+        self.lineEdit.setText(content)
 
 
     def get_key(self,title,role):
